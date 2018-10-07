f673a142d848c577540fe6795999d22d41ce5572
laiandrew2002 <laiandrew2002@users.noreply.github.com>
2018-10-08 05:21:43 +0800

    Updates

---
+++ precache-manifest.3abb4d40a6ec3aece7bff75a2cb2ca40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd5b3b6b432921443a6d",
+    "url": "/news-app/static/js/runtime~main.dd5b3b6b.js"
+  },
+  {
+    "revision": "a3d796684693cc12e4e8",
+    "url": "/news-app/static/js/main.a3d79668.chunk.js"
+  },
+  {
+    "revision": "6b83071d3843b5a24b53",
+    "url": "/news-app/static/js/1.6b83071d.chunk.js"
+  },
+  {
+    "revision": "a3d796684693cc12e4e8",
+    "url": "/news-app/static/css/main.e92f2d38.chunk.css"
+  },
+  {
+    "revision": "6b83071d3843b5a24b53",
+    "url": "/news-app/static/css/1.76d8a0c7.chunk.css"
+  },
+  {
+    "revision": "e65fe86365051cb4fd389b9cf6643c7a",
+    "url": "/news-app/index.html"
+  }
+];
